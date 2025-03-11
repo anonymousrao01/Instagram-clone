7bd444261a221e21088f2598220c2e9d7ef8f2b5
Muhammad Alyaan Aslam <raoalyanaslam0@gmail.com>
2025-03-11 16:05:24 +0500

    All code Updated

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const InstagramClone());
+}
+
+class InstagramClone extends StatelessWidget {
+  const InstagramClone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
